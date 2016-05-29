8f82147728808199412bc194a0e14501c6c73737
JImmy <jimmyleehales@gmail.com>
2016-05-29 11:57:42 -0700

    changed image to pic of jimmy

--- core/src/com/halmer/roommates/Roommates.java
+++ core/src/com/halmer/roommates/Roommates.java
@@ -13,7 +13,7 @@ public class Roommates extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("jimmy.jpg");
 	}
 
 	@Override
